452076800408d86a806df677731de85720ab41bf
Ryan Pineo <ryanpineo@gmail.com>
2015-04-21 10:48:13 -0400

    move README to README.rst

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='django-rest-framework-timedeltafield',
-    version='0.0.1',
+    version='0.0.2',
     description='Django Rest Framework serializer field for timedelta fields.',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     url='https://github.com/RyanPineo/django-rest-framework-timedelta-field',
     author='Ryan Pineo',
     author_email='ryanpineo@gmail.com',
